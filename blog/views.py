a66b78adaedd43f5535e9a87c954f0cad2a8166e
semen <ch-semen@rambler.ru>
2024-09-29 12:43:14 +0300

    chapter 2 end

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Post
 from django.http import Http404
 from django.shortcuts import get_object_or_404,render
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
 
 # Create your views here.
@@ -48,11 +49,17 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
 
     return render(
         request,
         'blog/post/detail.html',
-        {'post':post}
+        {
+            'post':post,
+            'comments' : comments,
+            'form' : form
+            }
     )
 
 def post_share(request, post_id):
@@ -94,5 +101,28 @@ def post_share(request, post_id):
             'post' : post,
             'form' : form,
             'sent' : sent
+        }
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status = Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post' : post,
+            'form' : form,
+            'comment' : comment
         }
     )
